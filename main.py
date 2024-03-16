0c53b6ecc9d26be15cabb4edea0ee4bfba7e73a6
emissan <ernesto.m.sanchez@outlook.com>
2024-03-15 20:29:47 -0400

    update the comments to better format and better describe what the code does

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 def main():
-    # specify the book to open
+    # Specify the book to open
     path_to_file = "books/frankenstein.txt"
     file_contents = print_book(path_to_file)
 
-    # call the word_count() function
+    # Call the word_count() function
     count = word_count(file_contents)
     
-    # call the character_count() funtion
+    # Call the character_count() funtion
     char = character_count(file_contents)
 
-    # call the report() function
+    # Call the report() function
     formatted_report = report(char)
     
-    # printing the report results
+    # Print the report results
     print("--- Begin report of books/frankenstein.txt ---")
     print(f"There were {count} words found in the book\n")
 
@@ -23,27 +23,27 @@ def main():
     print("--- End report ---")
     
 def print_book(book):
-    # open the specified file and prints out the contents
+    # Open the specified file and print out the contents
     with open(book) as f:
         file_contents = f.read()
     return file_contents
 
 def word_count(file):
-    # seperate each individual word in the book
+    # Seperate each individual word in the book
     words = file.split()
     
-    # count the amount of words in the file_contents
+    # Count the amount of words in the book
     count = len(words)
     return count
 
 def character_count(document):
-    # make all letters lowercase
+    # Make all letters lowercase
     lowered_document = document.lower()
     
-    # create a dictionary to hold each character and count
+    # Create a dictionary to hold letter and occurence count
     characters = {}
 
-    # populate the dictionary by counting letters
+    # Populate the dictionary by counting letters only
     for char in lowered_document:
         if char.isalpha():
             if char in characters:
@@ -53,10 +53,10 @@ def character_count(document):
     return characters
 
 def report(unformatted_text):
-    # sort the list of dictionaries by highest value
+    # Sort the dictionary by highest value
     sorted_list = dict(sorted(unformatted_text.items(), key=lambda x: x[1], reverse=True))
 
-    # convert the original dictionary to a list of dictionaries
+    # Convert the original dictionary to a list of dictionaries
     char_list = [{key: value} for key, value in sorted_list.items()]
     return char_list
 
